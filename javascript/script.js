87510d30dd36e122f34d016035e2425d14d9f5df
jessica-nardy <nardy.arq@gmail.com>
2025-02-08 17:01:48 +0000

    Added current location

--- javascript/script.js
+++ javascript/script.js
@@ -29,10 +29,13 @@ setInterval(updateTime, 1000);
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#city-select");
-  citiesElement.innerHTML += `    <div class="city">
+  citiesElement.innerHTML = `    <div class="city">
                 <div>
                     <h2>${cityName}</h2>
                     <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
