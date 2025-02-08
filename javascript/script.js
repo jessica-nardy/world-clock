fa827e2c99fa9e4398ad9c9b06589eaa7c26d364
jessica-nardy <nardy.arq@gmail.com>
2025-02-08 16:02:35 +0000

    Added the functionality to the select

--- javascript/script.js
+++ javascript/script.js
@@ -23,5 +23,25 @@ function updateTime() {
     .tz("Asia/Tokyo")
     .format("h:mm:ss [<small>]A[</small>]");
 }
+
 updateTime();
 setInterval(updateTime, 1000);
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#city-select");
+  citiesElement.innerHTML += `    <div class="city">
+                <div>
+                    <h2>${cityName}</h2>
+                    <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+                </div>
+                <div class="time">${cityTime.format(
+                  "h:mm:ss"
+                )} <small>${cityTime.format("A")}</small></div>
+            </div>`;
+}
+
+let citiesSelect = document.querySelector("#cities");
+citiesSelect.addEventListener("change", updateCity);
