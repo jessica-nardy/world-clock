4d571e3f6897c6844d610b4cb7ed1f3955696bbd
jessica-nardy <nardy.arq@gmail.com>
2025-02-09 18:26:55 +0000

    Updated border-bottom color

--- javascript/script.js
+++ javascript/script.js
@@ -43,7 +43,7 @@ function updateCity(event) {
                 <div class="time">${cityTime.format(
                   "h:mm:ss"
                 )} <small>${cityTime.format("A")}</small></div>
-            </div>
+            </div> <br />
             <a href="/">All cities</a>`;
 }
 
